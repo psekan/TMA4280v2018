fd04ece2b5e6042b6f8c866d398a4b85bb72709a
MIchal Savčinský <michal.savcinsky@gmail.com>
2018-03-12 20:26:53 +0100

    zeta1-reduction added

---
+++ P1/zeta1-reduc/main.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include "../zeta1/computation/riemann.h"
+
+#define PI 3.14159265358979323846
+
+// count number of elements for each child process 
+// distribute data uniformly
+int dataDistribution(int numProcs, int n, int toAdd){ 
+  float tmp;
+  for (int i=0; i < n; i += toAdd){
+    tmp = (n - i) % numProcs;
+    numProcs--;
+    if (tmp == 0) {
+	return i;
+    }
+  }
+  return 0;
+}
+
+
+
+int main(int argc, char **argv){
+
+ //unsigned int n = atoi(argv[1]);
+
+  MPI_Status status;
+  MPI_Init(&argc, &argv);
+
+  double partialResult;
+  int tag = 100;
+  int rootProcess = 0;
+  int rank;
+  int numProcs;
+  double result=0;
+  int n;
+  double startTime, endTime;
+  //int elementsToSend;i
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+
+  if ( rank == rootProcess) {
+    startTime = MPI_Wtime();
+    if ((numProcs - 1) & (numProcs - 2) != 0) {
+      printf("Number of processes needs to be power of 2 + 1.");
+      return 1;
+    }
+    //double result=0;
+    printf("Insert precision n:\n");
+    scanf("%d", &n);
+
+    int avgElements = n/(numProcs-1);
+    int *dataArray=malloc(sizeof(int)*n);
+
+    int breakPoint = dataDistribution(numProcs-1, n, avgElements+1);
+    //generating data vector
+    for(unsigned i = 0; i < n; i++){
+      dataArray[i] = i+1;
+    }
+   
+    int i = 0;
+    int elementsToSend;
+
+    for(unsigned int globalRank=0; globalRank < n; globalRank+=elementsToSend){
+      if (globalRank < breakPoint) elementsToSend = avgElements + 1;
+      else elementsToSend = avgElements;
+      // distribute data to each child process
+      MPI_Send( &elementsToSend, 1, MPI_INT, i+1, tag, MPI_COMM_WORLD);
+      MPI_Send( dataArray + globalRank, elementsToSend, MPI_INT, i+1, tag, MPI_COMM_WORLD);
+      i++;
+    }
+
+    // collect results from each child process
+//    for (int i=1; i < numProcs; i++) {
+//      MPI_Recv(&partialResult, 1, MPI_DOUBLE, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
+//      result += partialResult;  
+//    }
+    free(dataArray);
+  }
+  else {
+    // child processes
+    int size;
+    MPI_Recv( &size, 1, MPI_INT, rootProcess, tag, MPI_COMM_WORLD, &status);
+    int *localArray=malloc(sizeof(int)*size);
+    MPI_Recv( localArray, size, MPI_INT,rootProcess, tag, MPI_COMM_WORLD, &status);
+    partialResult = 0.0;
+    for (unsigned int i=0; i < size; i++){
+      partialResult += piRiemannPart(localArray[i]);
+    }
+    //MPI_Send( &partialResult, 1, MPI_DOUBLE, rootProcess, tag, MPI_COMM_WORLD);
+    free(localArray);
+  }
+  
+  MPI_Allreduce(&partialResult, &result, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  if ( rank == rootProcess) {
+    endTime = MPI_Wtime();
+    printf("Procs: %d\nn: %d\nWall time: %f\nError: %.15f\n", numProcs, n, endTime - startTime, PI - piFromRiemann(result));
+  }
+  MPI_Finalize();
+  return 0;
+}
