e2922355412ae10d6c3468fd5e1a67cfccf6c557
Peter Sekan <433390@mail.muni.cz>
2018-03-14 20:08:06 +0100

    zeta fix 2

--- P1/zeta0/main.c
+++ P1/zeta0/main.c
@@ -4,12 +4,9 @@
 #include "computation/serial.h"
 
 int main(int argc, char ** argv) {
-    if (argc == 1) {
-        printf("Usage: %s <n>\n", argv[0]);
-        return 1;
-    }
-    unsigned int n = atoi(argv[1]);
-
+    unsigned int n;
+    printf("Insert precisition n: ");
+    scanf("%d", &n);
     double rzpi = piRiemannSerial(n);
 
     printf("RiemannZeta(%u) = %.15f\n", n, rzpi);
